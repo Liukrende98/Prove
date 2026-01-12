b43af3cad1b35a1580ebf7688fdb35be493efae1
Liukrende98 <liuk.rende98@gmail.com>
2026-01-12 14:40:17 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,4 @@
+// ========== CONFIGURAZIONE SUPABASE ==========
+const SUPABASE_URL = "https://xoagoblnzvdqnktpfhlk.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Uf5-1uzQzL-OqI6dA-tl_g_RHpYxNds";
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
